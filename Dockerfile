5bc5c1becfa6572bec5c199b8f7b7429550b91a0
Y-uri-K <muza131005@gmail.com>
2026-01-20 02:26:58 +1000

    INIT:initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9
+
+RUN pip install uv
+
+COPY pyproject.toml .
+COPY uv.lock .
+RUN uv sync --frozen
+
+ENV PATH = "/.venv/bin:$PATH"
+
+COPY . .
+
+CMD ["uv", "run", "python", "main.py"]
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    def __init__(self):
+        self.POSTGRES = {
+            "user": os.getenv("POSTGRES_USER", "bot_user"),
+            "password": os.getenv("POSTGRES_PASSWORD", "bot_password"),
+            "host": os.getenv("POSTGRES_HOST", "localhost"),
+            "port": os.getenv("POSTGRES_CONNECTION_PORT", "5432"),
+            "db": os.getenv("POSTGRES_DB", "bot_db"),
+        }
+
+        self.DATABASE_URL = (
+            f"postgresql+psycopg2://{self.POSTGRES['user']}:{self.POSTGRES['password']}"
+            f"@{self.POSTGRES['host']}:{self.POSTGRES['port']}/{self.POSTGRES['db']}"
+        )
+
+config = Config()
