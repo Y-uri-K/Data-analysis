b1e8d5d6ef0ffb0145d3a3fadd2ee5d2ba062a54
Y-uri-K <muza131005@gmail.com>
2026-01-22 12:18:25 +1000

    FIX: fix main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from generator.data_generator import DataGenerator
 def main() -> None:
     generator = DataGenerator()
     while True:
-        result = generator.generate_and_save(players_count=5, events_per_player=15)
+        result = generator.generate_and_save(players_count=5, events_per_player=5)
         print(f"Создано игроков: {result['players_count']}, событий: {result['events_count']}", flush=True)
         sleep(1)
 
